b85b32ffcc55475450fc48e2e47ca7eff19fbc6f
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-12 10:46:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from streamlit_oauth import OAuth2Component
 from utils.auth import pcoAuth
-st.title("hello")
 
 
-pcoAuth()
+st.title("Planning Center Analytics")
+
+
+with st.sidebar:
+  pcoAuth()
