48acc0d8ab9e33058e217f410815621603051c44
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-12 13:26:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from streamlit_oauth import OAuth2Component
 from utils.auth import pcoAuth
+import requests
 
 
 st.title("Planning Center Analytics")
 
 
 with st.sidebar:
-  pcoAuth()
+  oauth2 = pcoAuth()
+
+st.write(oauth2.get('https://api.planningcenteronline.com/people/v2/people')
