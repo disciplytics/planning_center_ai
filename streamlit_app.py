714e09d18acf5dee434769a276773901f890443d
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-12 14:15:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,5 @@ else:
   people = pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus')
   person = next(people)
   
-  st.write(len(people))
+  st.write(people)
   
