7174f5084cadf799aa43ed7180e2b3503a465f4c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-12 13:26:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ st.title("Planning Center Analytics")
 with st.sidebar:
   oauth2 = pcoAuth()
 
-st.write(oauth2.get('https://api.planningcenteronline.com/people/v2/people'))
+st.write(oauth2)#.get('https://api.planningcenteronline.com/people/v2/people'))
