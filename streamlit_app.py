0146ec518024d865a5dedb15fdc7b617a3da75f4
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-12 14:06:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ pco = pypco.PCO(token=st.session_state.token['access_token'])
 # Now, you're ready to go.
 # The iterate() function provides an easy way to retrieve lists of objects
 # from an API endpoint, and automatically handles pagination
-people = pco.iterate('/people/v2/people?include=households')
+people = pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus')
 person = next(people)
 
 st.write(people)
