0ec26dad946ebd0881fb727b5078e618644dcbab
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-14 15:52:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ else:
   # The iterate() function provides an easy way to retrieve lists of objects
   # from an API endpoint, and automatically handles pagination
   
-  load_data()
+  st.write(load_data())
   
 #st.json(people_df)
 #st.json(households_df)
