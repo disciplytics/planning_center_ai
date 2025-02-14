66023f2cc37628285761d5fe1455c7cc1911b259
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-14 15:49:55 -0500

    Create load_data.py

---
+++ utils/load_data.py
@@ -0,0 +1,20 @@
+def load_data():
+  import asyncio
+  async def fetch_people_data():
+    people_df = []
+    for person in pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus'):
+      people_df.append(person)
+    return people_df
+  
+  async def fetch_households_data():
+    households_df = []
+    for household in pco.iterate('/people/v2/households?include=people'):
+      households_df.append(household)
+    return households_df
+  
+  async def main():
+    await fetch_people_data()
+    await fetch_households_data()
+  
+  if __name__ == "__main__":
+    asyncio.run(main())
