466732937daae79b9aebed5966c9b95d0fcb426d
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-14 15:24:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,11 @@ else:
   # The iterate() function provides an easy way to retrieve lists of objects
   # from an API endpoint, and automatically handles pagination
   people = pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus')
-  person = next(people)
-  
-  st.write(len(people))
+  #person = next(people)
+
+  people_df = {}
+  for person in pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus'):
+       print(person)
+          
+
   
