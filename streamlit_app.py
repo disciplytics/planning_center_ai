5f94539be241ff8ba2a54558ff8ae8464809fa5d
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-14 15:47:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,15 +29,15 @@ else:
   # from an API endpoint, and automatically handles pagination
   
   async def fetch_people_data():
-          people_df = []
-          for person in pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus'):
-               people_df.append(person)
+    people_df = []
+    for person in pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus'):
+        people_df.append(person)
     return people_df
 
   async def fetch_households_data():
-          households_df = []
-          for household in pco.iterate('/people/v2/households?include=people'):
-               households_df.append(household)
+     households_df = []
+     for household in pco.iterate('/people/v2/households?include=people'):
+       households_df.append(household)
     return households_df
 
    async def main():
