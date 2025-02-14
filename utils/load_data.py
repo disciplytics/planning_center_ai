2fd56bbc72ffb1db0bce0d6dab97e43e3b414d3c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-14 16:06:36 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,5 +1,7 @@
-def load_data():
+import asyncio
 
+def load_data():
+  
   async def fetch_people_data():
     people_df = []
     for person in pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus'):
