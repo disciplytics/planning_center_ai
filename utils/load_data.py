19f6e19997f5ac9e6782de06a6573e0f30a8ad0c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-20 16:07:45 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -29,7 +29,7 @@ def load_data(pco):
     except Exception as e:
       # handle the exception
       error = f'{e.status_code}\n-\n{e.message}\n-\n{e.response_body}'
-      return e.message
+      return e.status_code
     
   async def main():
     return await fetch_people_data(), await fetch_households_data(), await fetch_donations_data()
