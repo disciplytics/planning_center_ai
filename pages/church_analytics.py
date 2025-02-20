2b09dc26203a8eebba387f69e3adf3a62e21f8bc
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-20 13:29:19 -0500

    Update and rename overview_analytics.py to church_analytics.py

--- pages/church_analytics.py
+++ pages/church_analytics.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # PAGE CONGIG
 st.set_page_config(
-        page_title="Planning Center Analytics", 
+        page_title="Church Analytics", 
         layout="wide",
         page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
 )
@@ -12,6 +12,6 @@ st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displ
         width = 250)
 
 # PAGE TITLE
-st.title("Planning Center Analytics :church:")
+st.title("Church Analytics :church:")
 
 st.dataframe(st.session_state.people_df)
