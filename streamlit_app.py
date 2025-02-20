561b83bd21e51eaafb681e44b1269b3d3054f202
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-20 13:33:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,5 @@
 import streamlit as st
 
-# PAGE CONGIG
-#st.set_page_config(
-#        page_title="Planning Center Analytics", 
-#        layout="wide",
-#        page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
-#)
-
-# LOGO
-#st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
-#        width = 250)
-
-# PAGE TITLE
-#st.title("Planning Center Analytics :church:")
-
 pco_integration = st.Page(
   'pages/pco_integration.py', 
   title = 'Planning Center Integration', 
@@ -32,8 +18,9 @@ church_analytics = st.Page(
 # navigation 
 pg = st.navigation(
         {
-            "Home": [pco_integration],
+            "Home": [],
             "Analytics": [church_analytics],
+            "Settings": [pco_integration],
           
         }
     )
