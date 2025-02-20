51d08bbacc57a3707874ce2d723982c958fba304
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-20 10:22:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,11 @@ import pandas as pd
 import asyncio
 from utils.load_data import load_data
 
-st.set_page_config(page_title="Planning Center Analytics App", layout="wide",)
+st.set_page_config(
+        page_title="Planning Center Analytics App", 
+        layout="wide",
+        page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
+)
 
 st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
         width = 250)
