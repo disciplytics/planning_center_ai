275edb050ce713f86b82e1affad06b6507182a7d
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 15:53:40 -0500

    Update attendance.py

--- reports/attendance.py
+++ reports/attendance.py
@@ -4,3 +4,4 @@ def attendance_trend(data):
   data['attributes.total'] = data['attributes.total'].astype('int32')
   data['attributes.starts_at_at'] = data['attributes.starts_at_at'].astype('datetime64[ns]')
   bar_chart(data=data, x='attributes.starts_at_at', y='attributes.total', x_label='Date', y_label='Headcounts', color='Headcount Type',)# horizontal=False, stack=None, width=None, height=None, use_container_width=True)
+  return data
