d4bc7740e7f3d626c50c328f557e014b8a6382c9
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 16:40:35 -0500

    Update church_analytics.py

--- pages/church_analytics.py
+++ pages/church_analytics.py
@@ -29,7 +29,7 @@ else:
         headcount_col, giving_col = st.columns(2)
         
         with headcount_col.container(border=True):
-                st.write("Headcount Metrics")
+                st.subheader("Headcount Metrics")
                 st.bar_chart(data=hc_trend_df, x='Date', y='Headcounts', x_label='Date', y_label='Headcounts', color='Headcount Type',)# horizontal=False, stack=None, width=None, height=None, use_container_width=True)
                 st.write(hc_trend_df)
                 st.write(st.session_state.headcounts_df)
