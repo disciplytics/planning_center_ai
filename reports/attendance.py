8d96a51e2a64019296776f0e6c229db86d9c1334
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 15:48:36 -0500

    Update attendance.py

--- reports/attendance.py
+++ reports/attendance.py
@@ -1,4 +1,6 @@
 from streamlit import bar_chart
 def attendance_trend(data):
   data['Headcount Type'] = data['attributes.name_at']
+  data['attributes.total'] = data['attributes.total'].astype('int32')
+  data['attributes.starts_at_at'] = data['attributes.starts_at_at'].astype('datetime64[ns]')
   return bar_chart(data=data, x='attributes.starts_at_at', y='attributes.total', x_label='Date', y_label='Headcounts', color='Headcount Type',)# horizontal=False, stack=None, width=None, height=None, use_container_width=True)
