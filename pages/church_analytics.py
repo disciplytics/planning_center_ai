d20d0cf8cbce5b3c75bd390ed90e7095a1595fda
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 16:17:00 -0500

    Update church_analytics.py

--- pages/church_analytics.py
+++ pages/church_analytics.py
@@ -21,7 +21,7 @@ else:
         #@st.cache_data
         def headcounts_trend(data):
                 data['Headcount Type'] = data['attributes.name_at']
-                data['Headcounts'] = pd.to_numeric(data['attributes.total'])#.astype('int32')
+                data['Headcounts'] = pd.to_numeric(data['attributes.total'].str[:10])#.astype('int32')
                 data['Date'] = pd.to_datetime(data['attributes.starts_at_at'])#.astype('datetime64[ns]')
                 return data[['Headcount Type', 'Date', 'Headcounts']]#.groupby(['Headcount Type', 'Date'])['Headcounts'].sum().reset_index()
         hc_trend_df = headcounts_trend(st.session_state.headcounts_df)
