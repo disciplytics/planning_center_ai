cc42e1f1ef40fa6fd2a8bee169be141f7756efeb
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 12:23:52 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -90,7 +90,7 @@ def load_data(pco):
                      'attributes.e164', 'attributes.international', 'attributes.national']
       """
       
-      return pd.json_normalize(people_data_df[['relationships.households.data']])#people_include_df[people_include_df['type'] == 'PhoneNumber'].dropna(axis=1, how='all').drop_duplicates()
+      return pd.json_normalize(people_data_df['relationships.households.data'])#people_include_df[people_include_df['type'] == 'PhoneNumber'].dropna(axis=1, how='all').drop_duplicates()
       
     except Exception as e:
       # handle the exception
