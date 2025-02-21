110ecd5c7f4aa0ee95faa1e414cc8c954c2c41cb
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 16:07:23 -0500

    Update church_analytics.py

--- pages/church_analytics.py
+++ pages/church_analytics.py
@@ -25,8 +25,10 @@ else:
                         data['Headcount Type'] = data['attributes.name_at']
                         data['attributes.total'] = data['attributes.total'].astype('int32')
                         data['attributes.starts_at_at'] = data['attributes.starts_at_at'].astype('datetime64[ns]')
+                        data = data.groupby(['Headcount Type', 'attributes.starts_at_at'])['attributes.total'].sum().reset_index()
                         return data
-                st.bar_chart(data=headcounts_trend(st.session_state.headcounts_df), x='attributes.starts_at_at', y='attributes.total', x_label='Date', y_label='Headcounts', color='Headcount Type',)# horizontal=False, stack=None, width=None, height=None, use_container_width=True)
+                hc_trend_df = headcounts_trend(st.session_state.headcounts_df)
+                st.bar_chart(data=, x='attributes.starts_at_at', y='attributes.total', x_label='Date', y_label='Headcounts', color='Headcount Type',)# horizontal=False, stack=None, width=None, height=None, use_container_width=True)
         
         
                 st.write(st.session_state.headcounts_df)
