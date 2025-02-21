8db28d7019c0321163232a4e160fef4616fde697
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 12:29:20 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -89,8 +89,8 @@ def load_data(pco):
                      'relationships.person.data.type', 'attributes.country_code', 'attributes.carrier',
                      'attributes.e164', 'attributes.international', 'attributes.national']
       """
-      
-      return pd.json_normalize(people_data_df['relationships.households.data'])#people_include_df[people_include_df['type'] == 'PhoneNumber'].dropna(axis=1, how='all').drop_duplicates()
+      people_data_df[['household_id', 'household_type']] = [itemgetter('id','type',)(x) for x in people_data_df['relationships.households.data'].to_list()]
+      return people_data_df#pd.json_normalize(people_data_df['relationships.households.data'])#people_include_df[people_include_df['type'] == 'PhoneNumber'].dropna(axis=1, how='all').drop_duplicates()
       
     except Exception as e:
       # handle the exception
