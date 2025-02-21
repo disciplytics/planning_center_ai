77bb4b0ec99daa844ae54cade93a79c71270d5df
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-21 13:11:09 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,129 +3,15 @@ import pandas as pd
 
 def load_data(pco):
   
-  async def fetch_people_data():
-    """
-      Household:
-      Where statement:
-      people_household_df = people_include_df[people_include_df['type'] == 'Household'].drop_duplicates()
-
-      Join:
-      people_data_df['relationships.households.data.id'] == people_household_df['id']
-      
-      Keep columns: ['id', 'attributes.member_count', 'attributes.primary_contact_id', 'attributes.primary_contact',
-                     'relationships.people.data', 'attributes.name']
-                     
-      Drop columns: ['type', 'attributes.created_at', 'attributes.updated_at', 'links.self', 'attributes.avatar',
-                     'relationships.primary_contact.data.type', 'relationships.primary_contact.data.id',]
-
-      Email:
-      Where statement:
-      people_email_df = people_include_df[people_include_df['type'] == 'Email'].drop_duplicates()
-
-      Join:
-      people_data_df['id'] == people_email_df['relationships.person.data.id']
-      
-      Keep columns: ['attributes.address', 'attributes.blocked', 'attributes.location', 
-                     'attributes.primary', 'relationships.people.data.id']
-                     
-      Drop columns: ['type', 'id', 'attributes.created_at', 'attributes.updated_at', 
-                     'links.self', 'relationships.person.data.type']
-
-      Address:
-      Where statement:
-      people_address_df = people_include_df[people_include_df['type'] == 'Address'].drop_duplicates()
-
-      Join:
-      people_data_df['id'] == people_address_df['relationships.person.data.id']
-
-      Keep columns: ['attributes.address', 'attributes.primary', 'relationships.people.data.id',
-                     'attributes.street_line_1', 'attributes.street_line_2', 'attributes.city',
-                     'attributes.state', 'attributes.zip', 'attributes.country_code', 'attributes.country_name']
-                     
-      Drop columns: ['type', 'id', 'attributes.created_at', 'attributes.updated_at', 'attributes.location', 
-                     'links.self', 'relationships.person.data.type']
-
-      MaritalStatus:
-      Where statement:
-      people_maritalstatus_df = people_include_df[people_include_df['type'] == 'MaritalStatus'].drop_duplicates()
-
-      Join:
-      people_data_df['relationships.marital_status.data.id'] == people_maritalstatus_df['id']
-      
-      Keep columns: ['id', 'attributes.value']
-                     
-      Drop columns: ['type', 'links.self']
-
-      Organization:
-      Where statement:
-      people_organization_df = people_include_df[people_include_df['type'] == 'Organization'].drop_duplicates()
-
-      Join:
-      people_data_df['relationships.organization.data.id'] == people_organization_df['id']
-      
-      Keep columns: ['id', 'attributes.name']
-                     
-      Drop columns: ['type', 'links.self', 'attributes.created_at', 'attributes.country_code',
-                     'attributes.date_format', 'attributes.time_zone', 'attributes.contact_website',
-                     'attributes.avatar_url']
-
-      PhoneNumber:
-      Where statement:
-      people_phonenumber_df = people_include_df[people_include_df['type'] == 'PhoneNumber'].drop_duplicates()
-
-      Join:
-      people_data_df['id'] == people_phonenumber_df['relationships.person.data.id']
-      
-      Keep columns: ['id', 'attributes.location', 'attributes.primary', 'relationships.person.data.id'
-                     'attributes.number']
-                     
-      Drop columns: ['type', 'id', 'links.self', 'attributes.created_at', 'attributes.updated_at',
-                     'relationships.person.data.type', 'attributes.country_code', 'attributes.carrier',
-                     'attributes.e164', 'attributes.international', 'attributes.national']
-    """
+  async def fetch_people_data(query_date: None):
     try:
       people_data_df = pd.DataFrame()
       people_include_df = pd.DataFrame()
       for person in pco.iterate('/people/v2/people?include=addresses,emails,field_data,households,inactive_reason,marital_status,organization,phone_numbers,primary_campus'):
         people_data_df = pd.concat([people_data_df, pd.json_normalize(person['data'])])
         people_include_df = pd.concat([people_include_df, pd.json_normalize(person['included'])])
-  
-      # explode household data
-      people_data_df = people_data_df.explode('relationships.households.data')
-      people_data_df = people_data_df.reset_index(drop=True)
-      people_data_df = pd.merge(
-                              people_data_df, 
-                              pd.json_normalize(people_data_df['relationships.households.data']).rename(columns={'type':'relationships.households.data.type','id':'relationships.households.data.id'}),
-                              left_index=True, 
-                              right_index=True
-                            )
-
-      # clean household data
-      people_household_df = people_include_df[people_include_df['type'] == 'Household'].drop_duplicates()
-      # people_household_df = people_household_df[['id', 'attributes.member_count', 'attributes.primary_contact_id', 'attributes.primary_contact', 'relationships.people.data', 'attributes.name']]
-      # do joins
-      # household data
-      people_data_df = pd.merge(
-            people_data_df,
-            people_household_df,
-            left_on = 'relationships.households.data.id',
-            right_on = 'id' 
-        )
-      del people_household_df
-                            
         
       return people_data_df
-    except Exception as e:
-      # handle the exception
-      error = f'{e.status_code}\n-\n{e.message}\n-\n{e.response_body}'
-      return e.status_code
-  
-  async def fetch_households_data():
-    try:
-      households_df = []
-      for household in pco.iterate('/people/v2/households?include=people'):
-        households_df.append(household['data']['attributes'])
-      return households_df
     except Exception as e:
       # handle the exception
       error = f'{e.status_code}\n-\n{e.message}\n-\n{e.response_body}'
