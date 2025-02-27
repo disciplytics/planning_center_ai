537ebb1e0f6be395839cec5fb202242485ccbe51
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-27 15:41:55 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -26,6 +26,19 @@ def load_data(pco):
 
       people_data_df = people_data_df.reset_index(drop=True)
       people_include_df = people_include_df.reset_index(drop=True)
+
+      people_data_df = people_data_df.explode('relationships.phone_numbers.data')        
+      people_data_df['relationships.phone_numbers.data.id'] = people_data_df['relationships.phone_numbers.data'].apply(lambda x: pd.Series(x['id']))
+
+      people_data_df = people_data_df.explode('relationships.households.data')        
+      people_data_df['relationships.households.data.id'] = people_data_df['relationships.households.data'].apply(lambda x: pd.Series(x['id']))
+
+      people_data_df = people_data_df.explode('relationships.emails.data')        
+      people_data_df['relationships.emails.data.id'] = people_data_df['relationships.emails.data'].apply(lambda x: pd.Series(x['id']))
+
+      people_data_df = people_data_df.explode('relationships.addresses.data')        
+      people_data_df['relationships.addresses.data.id'] = people_data_df['relationships.addresses.data'].apply(lambda x: pd.Series(x['id']))
+      
       return people_data_df
     except Exception as e:
       # handle the exception
