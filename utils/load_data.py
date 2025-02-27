ec89aae10b218dafeecd5ef3ddfb5c10ed0ad6d5
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-27 16:22:20 -0500

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -29,7 +29,7 @@ def load_data(pco):
 
       hh_df = people_include_df[people_include_df['type']=='Household'].dropna(axis=1, how='all')
       hh_df = hh_df.explode('relationships.people.data')        
-      #hh_df[['relationships.people.data.type', 'relationships.people.data.id']] = 
+      hh_df['relationships.people.data.id'] = pd.json_normalize(hh_df['relationships.people.data'])
       
 
       #people_data_df = pd.merge(people_data_df,hh_df,
@@ -37,7 +37,7 @@ def load_data(pco):
       #
 
       
-      return pd.json_normalize(hh_df)
+      return hh_df
     except Exception as e:
       # handle the exception
       error = f'{e.status_code}\n-\n{e.message}\n-\n{e.response_body}'
