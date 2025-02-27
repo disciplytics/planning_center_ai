8e2346a5097e26a548f5b10fd26614be66eb3c6e
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-27 09:00:13 -0500

    Update church_analytics.py

--- pages/church_analytics.py
+++ pages/church_analytics.py
@@ -124,4 +124,4 @@ else:
 
         df = st.session_state.donations_df
         st.write(pd.json_normalize(df.explode('relationships.designations.data')['relationships.designations.data']))
-        st.write(df.explode('relationships.designations.data')['relationships.designations.data'])
+        st.write(df.explode('relationships.designations.data'))
