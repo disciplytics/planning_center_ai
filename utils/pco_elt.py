1e131385054576fffc068e84bdad37f120859e43
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-02-28 18:40:17 -0500

    Update pco_elt.py

--- utils/pco_elt.py
+++ utils/pco_elt.py
@@ -27,7 +27,7 @@ def pco_elt(pco):
       people_data_df = people_data_df.reset_index(drop=True)
       people_include_df = people_include_df.reset_index(drop=True)
       
-      return pd.json_normalize(person['data'])
+      return person['data']
     except Exception as e:
       # handle the exception
       error = f'{e.status_code}\n-\n{e.message}\n-\n{e.response_body}'
