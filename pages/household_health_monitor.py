9b7d46770afc084082563120e0a5d02181a64fa2
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-03-07 09:56:37 -0500

    Update household_health_monitor.py

--- pages/household_health_monitor.py
+++ pages/household_health_monitor.py
@@ -25,7 +25,7 @@ else:
         pco = pypco.PCO(token=st.session_state.token['access_token'])
         st.write('Household Health Report')
 
-        st.json(pco_elt(pco))
+        st.json(st.session_state.token['access_token'])
         @st.cache_data
         def household_health_report():
                 return 'hello'
