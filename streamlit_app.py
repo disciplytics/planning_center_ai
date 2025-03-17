d07c3c87176da1f6fe371d3a73b8a043a3ee5fc4
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-03-16 21:15:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+if not st.experimental_user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.experimental_user.name}!")
+
 pco_integration = st.Page(
   'pages/pco_integration.py', 
   title = 'Planning Center Integration', 
