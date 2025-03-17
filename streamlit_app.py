346b92b69d2a89b393826f40f281ad90210cee94
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-03-16 21:17:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,4 @@ pg = st.navigation(
         }
     )
 
-
-if not st.experimental_user.is_logged_in:
-    if st.button("Log in"):
-        st.login()
-else:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")
-
 pg.run()
