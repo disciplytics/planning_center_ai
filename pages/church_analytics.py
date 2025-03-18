3d193a1b9b583b39754a00967ae9fda7a3108913
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-03-17 20:17:22 -0400

    Update church_analytics.py

--- pages/church_analytics.py
+++ pages/church_analytics.py
@@ -17,16 +17,3 @@ st.set_page_config(
 # LOGO
 st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
         width = 250)
-
-# Handle Streamlit's native authentication
-if not st.experimental_user.is_logged_in:
-    if st.button("Log in using Streamlit's native authentication"):
-        st.login()
-else:
-    # Add subscription check for logged-in users
-    add_auth()
-    
-    # Your app code here - only runs for subscribed users
-    st.write('Stuff')
-
-
